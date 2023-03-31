0f895f2290642200d3d5f12c5f0b10801762c2e0
Daniel Boll <danielboll.academico@gmail.com>
2023-03-31 15:55:41 -0300

    fix(test): removing test
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,8 @@
 set_languages("c++20")
 add_rules("mode.debug", "mode.release")
 
-local libs = { "gtest" }
-
 add_includedirs("include")
-add_requires(table.unpack(libs))
 
 target("penis-lib")
   set_kind("static")
   add_files("src/**/*.cpp")
-  add_packages(table.unpack(libs))
-
-target("test")
-  set_kind("binary")
-  add_files("test/*.cpp")
-  add_packages(table.unpack(libs))
-  add_deps("penis-lib")
