fa2c7a46b8e313dc0c95a96cccf67d174d9712e2
Daniel Boll <danielboll.academico@gmail.com>
2023-03-31 23:07:00 -0300

    feat(lib): trying to make penis available
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,10 @@
+set_project("penis")
+set_version("1.0.0")
+set_xmakever("2.7.0")
+
+-- set_warnings("all", "error")
 set_languages("c++20")
+
 add_rules("mode.debug", "mode.release")
 
 add_includedirs("include")
@@ -6,3 +12,5 @@ add_includedirs("include")
 target("penis-lib")
   set_kind("static")
   add_files("src/**/*.cpp")
+
+add_installfiles("(include/**)", {prefixdir = ""})
